19e41bba6d8ac6e82ef58191e99ff5c2a7bb0c1d
Jdpa357 <jdpa352@hotmail.es>
2019-12-12 22:56:35 +0000

    Adding last mandatory tasks and fixes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-if len(sys.argv) is 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-for i, a in enumerate(sys.argv):
-    if i > 0:
-        print("{}: {}".format(i, a))
+if __name__ == '__main__':
+    import sys
+    if len(sys,argv) == 1:
+	print("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+	print("{} argument:".format(len(sys.argv) - 1))
+    else:
+	print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv)):
+	if i == 0:
+	    continue
+	print("{:d}: {:s}".format(i, sys.argv[i]))
