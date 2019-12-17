145f7a1a93fc2cd2873672724d46d09cb175071a
Jdpa357 <jdpa352@hotmail.es>
2019-12-17 18:20:08 +0000

    Fixing more test cases

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
+    if idx >= len(my_list) or idx < 0:
         return None
     else:
         return my_list[idx]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
-        return my_list
-    else:
+    if len(my_list) > idx >= 0:
         my_list[idx] = element
     return my_list
