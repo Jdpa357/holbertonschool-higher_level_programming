18e9452635531613a3001840adc92115adcaf9e9
Jdpa357 <jdpa352@hotmail.es>
2019-12-18 00:39:34 +0000

    Fixing logic

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) or len(tuple_b) < 2:
-        for i in range(2):
-            tuple_a = tuple_a + (0,)
-            tuple_b = tuple_b + (0,)
-    tuple_a = (tuple_a[0], tuple_a[1])
-    tuple_b = (tuple_b[0], tuple_b[1])
-    result_a = tuple_a[0] + tuple_b[0]
-    result_b = tuple_a[1] + tuple_b[1]
-    total_result = (result_a, result_b)
-    return total_result
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
