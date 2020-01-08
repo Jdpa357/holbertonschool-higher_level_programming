f98b3058bd23edc33ade389a725cfa08f43be5fd
Jdpa357 <jdpa352@hotmail.es>
2020-01-08 21:00:01 +0000

    Adding new tasks to project

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,11 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-        print("Inside result: {:}".format(result))
-        return result
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        except (TypeError, ValueError):
+            pass
+    print("")
+    return count
