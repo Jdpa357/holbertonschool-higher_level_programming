84daf9609833911029ac5bdd2397cd9e3e28c45c
Jdpa357 <jdpa352@hotmail.es>
2020-01-08 13:47:06 +0000

    Adding first task to project

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+	try:
+		result = my_list.__len__()
+		print('%s' % ''.join(map(str, my_list[:x])))
+		return (result)
+	except IndexError:
+		print(*my_list, sep = "")
