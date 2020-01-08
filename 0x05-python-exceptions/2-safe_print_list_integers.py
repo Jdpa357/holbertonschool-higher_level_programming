af2b3ae8ee6112232fde132d17fb1c03680dcec8
Jdpa357 <jdpa352@hotmail.es>
2020-01-08 20:53:02 +0000

    Adding new tasks to project

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {:}".format(result))
+        return result
