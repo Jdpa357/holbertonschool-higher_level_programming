dd48339fa266359090ff2d4e7f0afacc840538cb
Jdpa357 <jdpa352@hotmail.es>
2020-01-08 21:10:08 +0000

    Adding last mandatory tasks to project

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+def list_division(my_list_1, my_list_2, list_length):
+    count = 0
+    i = 0
+    new_list = []
+    for i in range(list_length):
+        try:
+            tmp = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            tmp = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            tmp = 0
+        except IndexError:
+            print("out of range")
+            r = 0
+        finally:
+            new_list.append(tmp)
+    return new_list
