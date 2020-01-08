c904c5fe781bb9862785187d1474c4d9141b00ba
Jdpa357 <jdpa352@hotmail.es>
2020-01-08 20:48:15 +0000

    Correcting pep8 compliance

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-	try:
-		result = my_list.__len__()
-		print('%s' % ''.join(map(str, my_list[:x])))
-	except IndexError:
-		pass
-	return (result)
+    result = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            result += 1
+        except IndexError:
+            continue
+    print("")
+    return (result)
