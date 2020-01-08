7256dd9e760c73dba204d72c9a40aa860d01ae62
Jdpa357 <jdpa352@hotmail.es>
2020-01-08 20:43:44 +0000

    Fixing exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,6 @@ def safe_print_list(my_list=[], x=0):
 	try:
 		result = my_list.__len__()
 		print('%s' % ''.join(map(str, my_list[:x])))
-		return (result)
-	except:
-		print("An unknow error")
+	except IndexError:
+		pass
+	return (result)
