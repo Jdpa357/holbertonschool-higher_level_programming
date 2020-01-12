e610a74c8e52cf3b50b4640915fbc9df1b4229d2
Jdpa357 <jdpa352@hotmail.es>
2020-01-12 22:03:16 +0000

    Changing logic of function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,9 +12,9 @@ def add_integer(a, b=98):
     integers or float
     """
 
-    if type(a) != int or type(a) != float:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) != int or type(b) != float:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     if type(a) == float:
