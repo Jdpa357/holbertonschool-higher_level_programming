54706d088288db123500c21c84d5e9747468a444
Jdpa357 <jdpa352@hotmail.es>
2020-01-13 17:22:03 +0000

    Fixing test suites

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,14 +16,14 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError('division by zero')
     if not isinstance(matrix, list):
         raise TypeError('matrix must be a matrix (list of lists)'
-                        'of integers/floats')
+                        ' of integers/floats')
     if not all(isinstance(rows, list) for rows in matrix):
         raise TypeError('matrix must be a matrix (list of lists)'
-                        'of integers/floats')
+                        ' of integers/floats')
     for rows in matrix:
         if not all(isinstance(cols, (float, int)) for cols in rows):
             raise TypeError('matrix must be a matrix (list of lists)'
-                            'of integers/floats')
+                            ' of integers/floats')
     if not all(len(rows) == len(matrix[0]) for rows in matrix):
         raise TypeError('Each row of the matrix must have the same size')
     result = [[round(cols/div, 2) for cols in rows] for rows in matrix]
