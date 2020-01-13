45ea3d3ae7348ffa2339534ff41abb31f1ae235e
Jdpa357 <jdpa352@hotmail.es>
2020-01-13 18:11:23 +0000

    Fixing test suite of last task with unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,9 +11,15 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([-2, 5, -8, 3]), 5)
         self.assertEqual(max_integer([1]), 1)
-        self.assertEqual(max_integer([5.6, 6, 8.9, 8.5]), 8.9)
+
+    def test_max_list_of_list(self):
         self.assertEqual(max_integer([[5, 6, 8], [1, 2, 4]]), [5, 6, 8])
+
+    def test_max_float(self):
+        self.assertEqual(max_integer([5.6, 6, 8.9, 8.5]), 8.9)
+
+    def test_max_empty(self):
         self.assertEqual(max_integer(), None)
 
-    with self.assertRaises(TypeError):
-        max_integer(['zaco', 'guest', 6])
+    def text_max_instance(self):    
+        self.assertNotIsInstance(max_integer([5, 9.2]), int)
