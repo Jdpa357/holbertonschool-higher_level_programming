ba6b5c36755f8c0b010b96c6406792cb300f94a1
Jdpa357 <jdpa352@hotmail.es>
2020-01-27 22:28:21 +0000

    Corrections to base model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,13 +9,14 @@ import json
 class Base():
     """Base Class"""
 
+    __nb_objects = 0
+
     def __init__(self, id=None):
-        __nb_objects = 0
         if id is not None:
             self.id = id
         else:
-            __nb_objects += 1
-            self.id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
