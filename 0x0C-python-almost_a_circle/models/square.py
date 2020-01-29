f630e155c3eb40850606b617e1782b14901b23eb
Jdpa357 <jdpa352@hotmail.es>
2020-01-29 01:15:31 +0000

    Adding new logic

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,46 +1,60 @@
 #!/usr/bin/python3
+
 """
 Class Square Module
 """
-from models.base import Base
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Class Square with inheritance from Base and Rectangle"""
+    """Class Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize"""
         super().__init__(size, size, x, y, id)
-        self.size = size
+
+    def __str__(self):
+        """ string"""
+        y = str(self.y)
+        x = str(self.x)
+        w = str(self.width)
+        i = str(self.id)
+        string = "[Square] (" + i + ") " + x + "/" + y + " - " + w
+        return string
 
     @property
     def size(self):
-        return self.__size
+        """Getter"""
+        return self.width
 
     @size.setter
-    def size(self, value):
-        self.__size = value
-        self.width = value
-        self.height = value
-
-    def __str__(self):
-        """Override to return the below"""
-        return '[Square] ({}) {}/{} - {}' \
-            .format(self.id, self.x, self.y, self.width)
+    def size(self, size):
+        """Setter"""
+        self.width = size
+        self.height = size
 
     def update(self, *args, **kwargs):
-        """Assigns an argument to each attribute"""
-        attrs = ['id', 'size', 'x', 'y']
-        arg_list = zip(attrs, args) if args else kwargs.items()
-        for k, v in arg_list:
-            if k in attrs:
-                setattr(self, k, v)
+        """update"""
+        if args:
+            a = ["id", "size", "x", "y"]
+            for i, e in enumerate(args):
+                setattr(self, a[i], e)
+            return
+        for x, y in kwargs.items():
+            if hasattr(self, x):
+                setattr(self, x, y)
 
     def to_dictionary(self):
-        """Returns the dictionary representation of Square"""
-        new_dict = {k.replace('_Square__', '').replace('_Rectangle__', ''): v
-                    for k, v in self.__dict__.items()}
-        del_keys = ['width', 'height']
-        for i in del_keys:
-            del new_dict[i]
-        return new_dict
+        """Dcitionary"""
+        Dictorionary = {}
+        for x, y in vars(self).items():
+            if x.startswith("_"):
+                if not x.endswith("width") and not x.endswith("height"):
+                    idx = x.index("__")
+                    Dictorionary[x[idx + 2:]] = y
+                else:
+                    Dictorionary["size"] = y
+            else:
+                Dictorionary[x] = y
+        return Dictorionary
