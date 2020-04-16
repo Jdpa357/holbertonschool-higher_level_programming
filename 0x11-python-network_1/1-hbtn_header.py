9c7e8ef58d2888252bc3070d768813a8d5e40e80
Jdpa357 <jdpa352@hotmail.es>
2020-04-16 15:59:12 +0000

    Hotfix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,7 @@ of the variable found in the header of the response
 import urllib.request
 import sys
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         print(response.info().get('X-Request-Id'))
