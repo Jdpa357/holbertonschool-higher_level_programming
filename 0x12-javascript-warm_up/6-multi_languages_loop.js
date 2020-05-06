6a658402543d8a01bb98d2c94db3ca0983ed9fc9
Jdpa357 <jdpa352@hotmail.es>
2020-05-06 14:07:21 +0000

    Adding const to file variable

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let myStringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myStringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < myStringArray.length; i++) {
   console.log(myStringArray[i]);
 }
