0d1c4fbdcd6d106d17f519d7fb0523e450399dab
Jdpa357 <jdpa352@hotmail.es>
2020-05-11 22:17:36 +0000

    Adding new tasks to project

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// This module defines a square class
+
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    const char = (c) || 'X';
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+};#!/usr/bin/node
