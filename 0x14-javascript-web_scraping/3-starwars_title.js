39a0992fbd031fd6395451c446a57b92998c0458
Jdpa357 <jdpa352@hotmail.es>
2020-05-12 21:22:34 +0000

    Adding different quotes

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/${process.argv[2]}', (error, response, body) => {
-  if (error) {
-    console.log(error);
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) {
+    console.log(err);
   } else {
     console.log(JSON.parse(body).title);
   }
