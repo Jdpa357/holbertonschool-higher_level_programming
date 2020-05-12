ce406151efd816492bb6cb1719a6b65157dc790f
Jdpa357 <jdpa352@hotmail.es>
2020-05-12 21:21:13 +0000

    Fix request url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, (error, response, body) => {
+request('https://swapi-api.hbtn.io/api/films/${process.argv[2]}', (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
